f17d9c8f5db21d98ae4f91476e9775ec61d28a01
Lixun Zhang <lixun.zhang@amd.com>
2024-10-23 08:14:27 -0500

    comment out

--- ops/triton/ragged_hstu_attention_bench.py
+++ ops/triton/ragged_hstu_attention_bench.py
@@ -168,9 +168,10 @@ def main(
     timestamps = generate_hstu_timestamps(batch_size, int(lengths.max().item()))
     relative_bias_type = "ALL"
 
-    Z = seq_offsets.numel() - 1
-    for i in range(Z):
-        seq_offsets[i] = i*2048
+    #Z = seq_offsets.numel() - 1
+    #for i in range(Z):
+    #    seq_offsets[i] = i*2048
+    #num_targets = None
 
     if not no_relative_bias:
         fn = lambda: _RaggedAttentionRelativeBiasFunction.apply(
